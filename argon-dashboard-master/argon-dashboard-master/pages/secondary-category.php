7b36e5ae08acbed5d8b09a123da966a4014dc3a9
Ann <forever970980@gmail.com>
2024-01-28 22:56:09 +0800

    次分類版面設置

---
+++ argon-dashboard-master/argon-dashboard-master/pages/secondary-category.php
@@ -0,0 +1,298 @@
+<?php
+require_once("../../../db_connect_midterm_project_v3.php");
+
+
+$sqlAll = "SELECT * FROM secondary_category where valid = 1";
+$resultAll = $conn->query($sqlAll);
+$perpage = 6;
+$categoryTotalCount = $resultAll->num_rows;
+$pageCount = ceil($categoryTotalCount / $perpage);
+
+if (isset($_GET["order"])) {
+  $order = $_GET["order"];
+  if ($order == 1) {
+    $orderstring = "ORDER BY id ASC";
+  }
+  if ($order == 2) {
+    $orderstring = "ORDER BY id DESC";
+  }
+  if ($order == 3) {
+    $orderstring = "ORDER BY name ASC";
+  }
+  if ($order == 4) {
+    $orderstring = "ORDER BY name DESC";
+  }
+}
+
+if (isset($_GET["search"])) {
+  $search = $_GET["search"];
+  $sql = "SELECT * FROM secondary_category WHERE name  LIKE '%$search%' AND valid = 1";
+} else if ($_GET["p"]) {
+  $p = $_GET["p"];
+  $startIndex = ($p - 1) * $perpage;
+  $sql = "SELECT * FROM secondary_category WHERE valid = 1 $orderstring LIMIT $startIndex , $perpage";
+} else {
+  $p = 1;
+  $order = 1;
+  $orderstring = "ORDER BY id ASC";
+  $sql = "SELECT * FROM secondary_category WHERE valid = 1 $orderstring LIMIT  $perpage";
+}
+?>
+<!--
+=========================================================
+* Argon Dashboard 2 - v2.0.4
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://www.creative-tim.com/license)
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <title>
+    Argon Dashboard 2 by Creative Tim
+  </title>
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <!-- Nucleo Icons -->
+  <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link id="pagestyle" href="../assets/css/argon-dashboard.css?v=2.0.4" rel="stylesheet" />
+  <!-- 自定義樣式 -->
+  <style>
+    .input-width {
+      width: 500px
+    }
+
+    .active {
+      color: #fff;
+    }
+  </style>
+</head>
+
+<body class="g-sidenav-show   bg-gray-100">
+  <div class="min-height-300 bg-primary position-absolute w-100"></div>
+  <main class="main-content position-relative border-radius-lg ">
+    <!-- Navbar -->
+    <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur" data-scroll="false">
+      <div class="container-fluid py-1 px-3">
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+            <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white" href="tables.html">主頁面</a></li>
+            <li class="breadcrumb-item text-sm text-white active" aria-current="page"><a class="opacity-5 text-white" href="primary-category.php">商品類別管理</a></li>
+            <li class="breadcrumb-item text-sm text-white active" aria-current="page"><a class=" text-white" href="javascript:;">次分類管理</a></li>
+          </ol>
+          <!-- <h6 class="font-weight-bolder text-white mb-0">主頁面</h6> -->
+        </nav>
+
+        <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+
+
+
+
+        </div>
+      </div>
+    </nav>
+
+    <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+      <h3 class="text-white ps-5">次分類管理</h3>
+      <div class="input-group ps-5 input-width">
+        <input type="search" class="form-control " placeholder="搜尋類別" name="search">
+        <!-- <input type="search" class="form-control" placeholder="Name" aria-label="Recipient's username" aria-describedby="button-addon2" name="search" <?php if (isset($_GET["search"])) : $searchValue = $_GET["search"]; ?> value="<?= $searchValue ?>" <?php endif; ?>> -->
+        <button class="input-group-text text-body" type="submit" id="button-addon"><i class="fa-solid fa-magnifying-glass fa-fw"></i></button>
+      </div>
+    </div>
+    <!-- End Navbar -->
+
+    <div class="container-fluid py-4">
+
+      <div class="row justify-content-center">
+        <div class="col-8 ">
+          <div class="d-grid gap-2 mb-2 d-md-flex justify-content-md-end">
+            <a class="btn btn-light mb-0 mt-1 py-2 text-end" <?php if ($order == 1) echo "active" ?> href="secondary-category.php?order=1&p=<?= $p ?>" type="button">id <i class="fa-solid fa-arrow-down-short-wide"></i></a>
+            <a class="btn btn-light mb-0 mt-1 py-2 text-end" <?php if ($order == 2) echo "active" ?> href="secondary-category.php?order=2&p=<?= $p ?>" type="button">id <i class="fa-solid fa-arrow-up-wide-short"></i></a>
+            <a class="btn btn-light mb-0 mt-1 py-2 text-end" <?php if ($order == 3) echo "active" ?>href="secondary-category.php?order=3&p=<?= $p ?>" type="button">name <i class="fa-solid fa-arrow-down-short-wide"></i></a>
+            <a class="btn btn-light mb-0 mt-1 py-2 text-end" <?php if ($order == 4) echo "active" ?> href="secondary-category.php?order=4&p=<?= $p ?>" type="button">name <i class="fa-solid fa-arrow-up-wide-short"></i></a>
+          </div>
+          <div class="card mb-4">
+            <div class="card-body px-0 pt-0 pb-2">
+              <div class="table-responsive p-0">
+                <table class="table align-items-center justify-content-center mb-0">
+                  <thead>
+                    <tr>
+                      <th class="text-uppercase text-secondary font-weight-bolder opacity-7">編號</th>
+                      <th class="text-uppercase text-secondary font-weight-bolder opacity-7 ps-2">分類項目</th>
+                      <th class="text-uppercase text-secondary font-weight-bolder text-center opacity-7 ps-2">編輯</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php
+                    $rows = $resultAll->fetch_all(MYSQLI_ASSOC);
+                    foreach ($rows as $product_category) :
+                    ?>
+                      <tr>
+                        <td>
+                          <div class="d-flex px-3">
+                            <div class="my-auto">
+                              <h6 class="mb-0 text-sm"><?= $product_category["id"] ?></h6>
+                            </div>
+                          </div>
+                        </td>
+                        <td>
+                          <p class="text-sm font-weight-bold mb-0"><?= $product_category["name"] ?></p>
+                        </td>
+                        <!-- <td>
+                          <span class="text-xs font-weight-bold">working</span>
+                        </td> -->
+                        <td class="align-middle text-center">
+                          <div class="d-flex align-items-center justify-content-center">
+                            <a name="" id="" class=" me-2 text-lg font-weight-bold" href="#" role="button"><i class="fa-solid fa-pen-to-square"></i></a>
+                          </div>
+                        </td>
+                      </tr>
+                    <?php endforeach ?>
+                  </tbody>
+                </table>
+              </div>
+
+            </div>
+          </div>
+
+
+          <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+            <a class="btn btn-primary mb-0 mt-1 py-2 text-end" href="" type="button"><i class="fa-solid fa-plus"></i> 新增類別</a>
+          </div>
+
+        </div>
+      </div>
+      <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-center">
+          <?php for ($i = 1; $i <= $pageCount; $i++) : ?>
+            <li  class="page-item <?php if ($i == $p) echo "active"; ?>" ><a class="page-link" href="secondary-category.php?order=<?=$order?>&p=<?=$i?>" ><?= $i?></a></li>
+          <?php endfor ?>
+        </ul>
+      </nav>
+      <footer class="footer pt-3  ">
+        <div class="container-fluid">
+          <div class="row align-items-center justify-content-lg-between">
+            <div class="col-lg-6 mb-lg-0 mb-4">
+              <!-- <div class="copyright text-center text-sm text-muted text-lg-start">
+                © <script>
+                  document.write(new Date().getFullYear())
+                </script>,
+                made with <i class="fa fa-heart"></i> by
+                <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a>
+                for a better web.
+              </div> -->
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  </main>
+  <div class="fixed-plugin">
+    <div class="card shadow-lg">
+      <div class="card-header pb-0 pt-3 ">
+        <div class="float-start">
+          <h5 class="mt-3 mb-0">Argon Configurator</h5>
+          <p>See our dashboard options.</p>
+        </div>
+        <div class="float-end mt-4">
+          <button class="btn btn-link text-dark p-0 fixed-plugin-close-button">
+            <i class="fa fa-close"></i>
+          </button>
+        </div>
+        <!-- End Toggle Button -->
+      </div>
+      <hr class="horizontal dark my-1">
+      <div class="card-body pt-sm-3 pt-0 overflow-auto">
+        <!-- Sidebar Backgrounds -->
+        <div>
+          <h6 class="mb-0">Sidebar Colors</h6>
+        </div>
+        <a href="javascript:void(0)" class="switch-trigger background-color">
+          <div class="badge-colors my-2 text-start">
+            <span class="badge filter bg-gradient-primary active" data-color="primary" onclick="sidebarColor(this)"></span>
+            <span class="badge filter bg-gradient-dark" data-color="dark" onclick="sidebarColor(this)"></span>
+            <span class="badge filter bg-gradient-info" data-color="info" onclick="sidebarColor(this)"></span>
+            <span class="badge filter bg-gradient-success" data-color="success" onclick="sidebarColor(this)"></span>
+            <span class="badge filter bg-gradient-warning" data-color="warning" onclick="sidebarColor(this)"></span>
+            <span class="badge filter bg-gradient-danger" data-color="danger" onclick="sidebarColor(this)"></span>
+          </div>
+        </a>
+        <!-- Sidenav Type -->
+        <div class="mt-3">
+          <h6 class="mb-0">Sidenav Type</h6>
+          <p class="text-sm">Choose between 2 different sidenav types.</p>
+        </div>
+        <div class="d-flex">
+          <button class="btn bg-gradient-primary w-100 px-3 mb-2 active me-2" data-class="bg-white" onclick="sidebarType(this)">White</button>
+          <button class="btn bg-gradient-primary w-100 px-3 mb-2" data-class="bg-default" onclick="sidebarType(this)">Dark</button>
+        </div>
+        <p class="text-sm d-xl-none d-block mt-2">You can change the sidenav type just on desktop view.</p>
+        <!-- Navbar Fixed -->
+        <div class="d-flex my-3">
+          <h6 class="mb-0">Navbar Fixed</h6>
+          <div class="form-check form-switch ps-0 ms-auto my-auto">
+            <input class="form-check-input mt-1 ms-auto" type="checkbox" id="navbarFixed" onclick="navbarFixed(this)">
+          </div>
+        </div>
+        <hr class="horizontal dark my-sm-4">
+        <div class="mt-2 mb-5 d-flex">
+          <h6 class="mb-0">Light / Dark</h6>
+          <div class="form-check form-switch ps-0 ms-auto my-auto">
+            <input class="form-check-input mt-1 ms-auto" type="checkbox" id="dark-version" onclick="darkMode(this)">
+          </div>
+        </div>
+        <a class="btn bg-gradient-dark w-100" href="https://www.creative-tim.com/product/argon-dashboard">Free Download</a>
+        <a class="btn btn-outline-dark w-100" href="https://www.creative-tim.com/learning-lab/bootstrap/license/argon-dashboard">View documentation</a>
+        <div class="w-100 text-center">
+          <a class="github-button" href="https://github.com/creativetimofficial/argon-dashboard" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star creativetimofficial/argon-dashboard on GitHub">Star</a>
+          <h6 class="mt-3">Thank you for sharing!</h6>
+          <a href="https://twitter.com/intent/tweet?text=Check%20Argon%20Dashboard%20made%20by%20%40CreativeTim%20%23webdesign%20%23dashboard%20%23bootstrap5&amp;url=https%3A%2F%2Fwww.creative-tim.com%2Fproduct%2Fargon-dashboard" class="btn btn-dark mb-0 me-2" target="_blank">
+            <i class="fab fa-twitter me-1" aria-hidden="true"></i> Tweet
+          </a>
+          <a href="https://www.facebook.com/sharer/sharer.php?u=https://www.creative-tim.com/product/argon-dashboard" class="btn btn-dark mb-0 me-2" target="_blank">
+            <i class="fab fa-facebook-square me-1" aria-hidden="true"></i> Share
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--   Core JS Files   -->
+  <script src="../assets/js/core/popper.min.js"></script>
+  <script src="../assets/js/core/bootstrap.min.js"></script>
+  <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
+  <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
+  <script>
+    var win = navigator.platform.indexOf('Win') > -1;
+    if (win && document.querySelector('#sidenav-scrollbar')) {
+      var options = {
+        damping: '0.5'
+      }
+      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+    }
+  </script>
+  <!-- Github buttons -->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+  <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="../assets/js/argon-dashboard.min.js?v=2.0.4"></script>
+</body>
+
+</html>
